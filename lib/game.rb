c48e5a218b64c772a294da7df2455b5469a89162
Kathleen Ly <lykathleen@outlook.com>
2022-03-26 22:26:25 +0000

    Wrote test and code to calculate score per frame

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class Game
-  attr_reader :pins_per_roll
+  attr_reader :pins_per_roll, :pins_per_frame
 
   def initialize
     @pins_per_roll = []
+    @pins_per_frame = []
   end
 
   def roll(pins)
     @pins_per_roll << pins
+  end
+
+  def score_per_frame
+    @pins_per_frame = @pins_per_roll.each_slice(2).map {|num| num.inject(:+) }
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,6 +9,7 @@ describe Game do
   # To keep track of my points
   # I would like to keep a scoreboard which stores the pins I've knocked down per frame
   context "store ko'd pins" do
+
     it "stores the ko'd pins of the first rolled ball within the first frame" do
       game.roll(2)
       expect(game.pins_per_roll).to eq [2]
@@ -21,6 +22,21 @@ describe Game do
       expect(game.pins_per_roll[1]).to eq 3
     end
 
+  # As a player,
+  # So that we can determine a winner
+  # I would like to calculate the total score of all the pins I've knocked down
+  context "calculate score"
+
+    it 'calculates the score of each frame' do
+      game.roll(2)
+      game.roll(3)
+      game.roll(3)
+      game.roll(5)
+      expect(game.score_per_frame).to eq [5, 8]
+    end
+    # it "should return a spare if first and second ball in frame knocked down all 10 pins"
+
+    # end
 
   end
 end
